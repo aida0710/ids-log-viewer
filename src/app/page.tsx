d533142c32446bb1a6ad643d31fcb3b13386b015
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-09-10 22:39:37 +0900

    真ん中に文字を寄せないように

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,5 @@ import dynamic from 'next/dynamic';
 const LogViewer: React.ComponentClass<{}> | React.FunctionComponent<{}> = dynamic(() => import('@/app/index/LogViewer'), {ssr: false});
 
 export default function Page() {
-    return (
-        <div className='text-center'>
-            <LogViewer />
-        </div>
-    );
+    return <LogViewer />;
 }
