31385e43fdbd3c18f302734c31c13d3b62a96929
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-09-23 11:35:01 +0900

    defaultカラーをホワイトに修正

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -11,7 +11,7 @@ export function Providers({children}: Readonly<{children: React.ReactNode}>) {
 
     return (
         <ThemeProvider
-            defaultTheme='dark'
+            defaultTheme='default'
             attribute='class'>
             <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
         </ThemeProvider>
